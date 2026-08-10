e6a8e94c2ac41ff715cb32f332daac189c7d5255
Alexander Holas <alexander.holas@mailbox.org>
2026-08-10 19:04:11 +0200

    Base redesign

--- site/hesma/views.py
+++ site/hesma/views.py
@@ -20,6 +20,8 @@ def create_blueprint(app):
     blueprint = Blueprint(
         "hesma",
         __name__,
+        static_folder="./static",
+        static_url_path="/static/hesma",
         template_folder="./templates",
     )
 
