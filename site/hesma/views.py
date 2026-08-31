08819ff87e39cc4902512575988851e98be1e0ed
Max Heller <max.heller@h-its.org>
2026-08-31 13:09:05 +0200

    Fixed privacy policy and legal notice not rendering.

--- site/hesma/views.py
+++ site/hesma/views.py
@@ -1,20 +1,8 @@
 """Additional views."""
 
-from flask import Blueprint, abort, render_template
-from invenio_pages.records.models import PageModel
+from flask import Blueprint, render_template
 
 
-def _render_page(url):
-    """Look up a PageModel by URL and render it, or 404 if missing."""
-    page = PageModel.query.filter_by(url=url).first()
-    if page is None:
-        abort(404)
-    return render_template(page.template_name, page=page)
-
-
-#
-# Registration
-#
 def create_blueprint(app):
     """Register blueprint routes on app."""
     blueprint = Blueprint(
@@ -25,13 +13,12 @@ def create_blueprint(app):
         template_folder="./templates",
     )
 
-    # Add URL rules
     @blueprint.route("/legalnotice")
     def legal_notice():
-        return _render_page("/legalnotice")
+        return render_template("hesma/legalnotice.html")
 
     @blueprint.route("/privacy")
     def privacy_policy():
-        return _render_page("/privacy")
+        return render_template("hesma/privacy.html")
 
     return blueprint
